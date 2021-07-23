5765e425939cb207ecef5eda94592437ab815ea9
mohamedisakr <mohamedisakr@gmail.com>
2021-07-23 14:42:38 +0200

    testing by comparing _id property

--- test/user/read.test.js
+++ test/user/read.test.js
@@ -1,7 +1,17 @@
 const User = require("../../src/models/user");
 
-describe("reading user", () => {
-  test("read existing user", () => {
-    expect(1 + 1).toBe(2);
+describe("reading user info from database", () => {
+  let newUser;
+
+  beforeEach(async () => {
+    newUser = new User({ name: "john" });
+    await newUser.save();
+    // done();
+  });
+
+  test("find all existing user with specific name", async () => {
+    const users = await User.find({ name: "john" });
+    console.log(users);
+    expect(newUser._id.toString()).toBe(users[0]._id.toString());
   });
 });
