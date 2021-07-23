727d0c2348faa186544cc51f9e453edabf1ee3f0
mohamedisakr <mohamedisakr@gmail.com>
2021-07-23 15:23:28 +0200

    finding a paricular record

--- test/user/read.test.js
+++ test/user/read.test.js
@@ -11,7 +11,13 @@ describe("reading user info from database", () => {
 
   test("find all existing user with specific name", async () => {
     const users = await User.find({ name: "john" });
-    console.log(users);
+    // console.log(users);
     expect(newUser._id.toString()).toBe(users[0]._id.toString());
+  });
+
+  test("find an existing user with specific id", async () => {
+    const user = await User.findById(newUser._id);
+    console.log(user);
+    expect(user.name).toBe("john");
   });
 });
