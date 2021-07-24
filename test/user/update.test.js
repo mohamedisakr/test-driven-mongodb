c7057bb4462072d5aec63ceace478182f8337e7b
mohamedisakr <mohamedisakr@gmail.com>
2021-07-24 08:25:21 +0200

    using model instance update

--- test/user/update.test.js
+++ test/user/update.test.js
@@ -1,7 +1,25 @@
 const User = require("../../src/models/user");
 
 describe("updating user", () => {
-  test("update existing user", () => {
-    expect(1 + 1).toBe(2);
+  let newUser;
+
+  beforeEach(async () => {
+    newUser = new User({ name: "john" });
+    await newUser.save();
+  });
+
+  /*
+  test("instance set & save", async () => {
+    newUser.set("name", "Alex");
+    await newUser.save();
+    const user = await User.findOne({ name: "Alex" });
+    expect(user).not.toBeNull();
+  });
+  */
+
+  test("model instance update", async () => {
+    await newUser.update({ name: "Alex" });
+    const user = await User.findOne({ name: "Alex" });
+    expect(user).not.toBeNull();
   });
 });
