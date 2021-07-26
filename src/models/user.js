f2b4d3d55a263e60aa299c87980300bc3a80ae91
mohamedisakr <mohamedisakr@gmail.com>
2021-07-26 13:28:48 +0200

    restructuring models

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const postSchema = require("./post");
+const { Schema } = mongoose.Schema;
+// const postSchema = require("./post");
 
 const props = {
   name: {
